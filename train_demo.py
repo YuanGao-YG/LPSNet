7daed4a59ee6a94c1bf1fcbee882c1a48b6b5742
YuanGao <114328932+YuanGao-YG@users.noreply.github.com>
2023-01-11 19:04:08 +0800

    Update train_demo.py

--- train_demo.py
+++ train_demo.py
@@ -1,4 +1,9 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Mar 21 20:48:05 2020
 
+@author: Administrator
+"""
 
 import os, time, scipy.io, shutil
 import torch
@@ -10,30 +15,32 @@ from torch.utils.data import DataLoader
 import numpy as np
 import cv2
 import scipy.misc
-from LYSNet import *
+from LPSLE_Model import *
 from makedataset import Dataset
 import utils_train
 from Test_SSIM import *
+from Syn_Type import *
 
 os.environ["CUDA_DEVICE_ORDER"] = "PCI_BUS_ID"
 os.environ["CUDA_VISIBLE_DEVICES"] = "0"
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
 def load_checkpoint(checkpoint_dir):
 	if os.path.exists(checkpoint_dir + 'checkpoint.pth.tar'):
 		model_info = torch.load(checkpoint_dir + 'checkpoint.pth.tar')
 		print('loading existing model ......', checkpoint_dir + 'checkpoint.pth.tar')
-		net = LYSNet()
+		net = LPSLE()
 		device_ids = [0]
-		model = nn.DataParallel(net, device_ids=device_ids).cuda()
+		model = nn.DataParallel(net, device_ids=device_ids).to(device)
 		model.load_state_dict(model_info['state_dict'])
 		optimizer = torch.optim.Adam(model.parameters())
 		optimizer.load_state_dict(model_info['optimizer'])
 		cur_epoch = model_info['epoch']
 		
 	else:
-		net = LYSNet()
+		net = LPSLE()
 		device_ids = [0]
-		model = nn.DataParallel(net, device_ids=device_ids).cuda()
+		model = nn.DataParallel(net, device_ids=device_ids).to(device)
 		optimizer = torch.optim.Adam(model.parameters(), lr=1e-3)
 		cur_epoch = 0
 		
@@ -59,57 +66,6 @@ def train_psnr(train_in,train_out):
 	psnr = utils_train.batch_psnr(train_in,train_out,1.)
 	return psnr
 
-def SyntheticHaze(img,depth):
-    
-    A = np.random.uniform(0.8,1.0)       
-    beta = np.random.uniform(0.1,0.6)
-    
-    T = np.exp(-beta*depth)
-    hazyimg = np.zeros((img.shape))
-
-    for i in range(3):  
-        hazyimg[i,:,:] = img[i,:,:]*T + A*(1-T)
-        
-    return hazyimg
-
-
-def SyntheticSand(img,depth):
-    
-    As = [  
-            [0.38824,0.58039,0.78431],
-            [0.23922,0.52157,0.77647],
-            [0.21569,0.44314,0.75686],
-            [0.52941,0.61176,0.73333],
-            [0.61176,0.66275,0.72549],
-            [0.33333,0.4549,0.72549],
-            [0.33725,0.55686,0.71765],
-            [0.29412,0.43529,0.7098],
-            [0.26275,0.47843,0.70196],
-            [0.38824,0.56863,0.70196],
-            [0.20784,0.44314,0.6549],
-            [0.38039,0.53725,0.64706],
-            [0.06275,0.2902,0.63137],
-            [0.22353,0.38824,0.59608],
-            [0.21961,0.47843,0.59216],
-            [0.21961,0.43137,0.54902],
-            [0.29804,0.40392,0.51765],
-            [0.20392,0.41176,0.5098],
-            [0.2902,0.43137,0.4902],
-            [0.41569,0.46275,0.48627],
-            [0.2,0.33725,0.43529]
-        ]
-    
-    A = As[np.random.randint(0,len(As))]       
-    beta = np.random.uniform(0.1,0.6)
-    
-    T = np.exp(-beta*depth)
-    sandimg = np.zeros((img.shape))
-
-    for i in range(3):  
-        sandimg[i,:,:] = (img[i,:,:]+ A[i]-1)*T + A[i]
-    
-    return sandimg
-
 
 def hwc_to_chw(img):
     return np.transpose(img, axes=[2, 0, 1])
@@ -120,25 +76,27 @@ def chw_to_hwc(img):
 
 if __name__ == '__main__': 	
 	checkpoint_dir = './checkpoint/'
-	test_dir = './dataset/Test'
+	test_dir1 = './dataset/Test_Lowlight'
+	test_dir2 = './dataset/Test_Hazey'    
 	result_dir = './result'
-	testfiles = os.listdir(test_dir)
-
+    
+	testfiles1 = os.listdir(test_dir1)
+	testfiles2 = os.listdir(test_dir2)
     
 	maxpool = nn.MaxPool2d(kernel_size=3,stride=2,padding=1)
     
 	print('> Loading dataset ...')
 	dataset = Dataset(trainrgb=True, trainsyn=True, shuffle=True)
-	loader_dataset = DataLoader(dataset=dataset, num_workers=0, batch_size=16, shuffle=True)
+	loader_dataset = DataLoader(dataset=dataset, num_workers=0, batch_size=16, shuffle=False)
 	count = len(loader_dataset)
 	
-	lr_update_freq = 30
+	lr_update_freq = 20
 	model,optimizer,cur_epoch = load_checkpoint(checkpoint_dir)
-
-	L2_loss = torch.nn.MSELoss(reduce=True, size_average=True).cuda()
-	color_loss = nn.CosineSimilarity(dim=1, eps=1e-6)
+    
+	L1_loss = torch.nn.L1Loss(reduce=True, size_average=True).to(device)
+	L2_loss = torch.nn.MSELoss(reduce=True, size_average=True).to(device)
 	
-	for epoch in range(cur_epoch,100):
+	for epoch in range(cur_epoch,200):
 		optimizer = adjust_learning_rate(optimizer, epoch, lr_update_freq)
 		learnrate = optimizer.param_groups[-1]['lr']
 		model.train()
@@ -149,27 +107,28 @@ if __name__ == '__main__':
 
 			img_c = torch.zeros(data[:,0:3,:,:].size())		
 			img_l = torch.zeros(data[:,0:3,:,:].size())
-			img_d = torch.zeros(data[:,0:1,:,:].size())
-            
+
 			for nx in range(data.shape[0]):             
-				img_c[nx,:,:,:] = data[nx,0:3,:,:]#.numpy()
-				img_d[nx,:,:,:] = data[nx,3:4,:,:]#.numpy()
-          
-			for nxx in range(data.shape[0]):
+				img_c[nx,:,:,:] = data[nx,0:3,:,:]
                 
-				sor = np.random.uniform(0,1)                    
-				if sor <= 0.5:
-					img_l[nxx] = torch.from_numpy(SyntheticHaze(img_c[nxx,:,:,:],img_d[nxx,:,:,:]))
-				else:
-					img_l[nxx] = torch.from_numpy(SyntheticSand(img_c[nxx,:,:,:],img_d[nxx,:,:,:]))
+			sor = np.random.uniform(0,1)                   
+			if sor<=0.5:
+				Type = 1
+				for nxx in range(data.shape[0]):
+					img_l[nxx] = data[nxx,3:6,:,:]
+                
+			else:
+				Type = 2
+				for nxx in range(data.shape[0]):
+					img_l[nxx] = Hazey(data[nxx,0:3,:,:])
 
 									
-			input_var = Variable(img_l.cuda(), volatile=True)        
-			target_final = Variable(img_c.cuda(), volatile=True)
+			input_var = Variable(img_l.to(device), volatile=True)
+			target_final = Variable(img_c.to(device), volatile=True)
 
-			eout = model(input_var)
+			eout = model(input_var,Type)
 
-			enloss = 0.8 * L2_loss(eout,target_final) + 0.2 * torch.mean(-1 * color_loss(eout,target_final))
+			enloss = 0.5*L2_loss(eout,target_final) + 0.5*L1_loss(eout,target_final)
 			optimizer.zero_grad()
 			#Doptimizer.zero_grad()
             
@@ -179,16 +138,18 @@ if __name__ == '__main__':
 			#Doptimizer.step()
             
 			SN1_psnr = train_psnr(target_final,eout)		           
-			print("[Epoch %d][%d/%d] lr :%f loss: %.4f PSNR_train: %.4f" %(epoch+1, i+1, count, learnrate, enloss.item(), SN1_psnr))
+			print("[Epoch %d][Type--%d--][%d/%d] lr :%f loss: %.4f PSNR_train: %.4f" %(epoch+1,Type, i+1, count, learnrate, enloss.item(), SN1_psnr))
 			
-		for f in range(len(testfiles)):
+		for f in range(len(testfiles1)):
 			model.eval()
 			with torch.no_grad():
-				img_ccc = cv2.imread(test_dir + '/' + testfiles[f]) / 255.0
+				img = cv2.imread(test_dir1 + '/' + testfiles1[f])
+				h,w,c = img.shape
+				img_ccc = cv2.resize(img,(512,512)) / 255.0
 				img_h = hwc_to_chw(img_ccc)
-				input_var = torch.from_numpy(img_h.copy()).type(torch.FloatTensor).unsqueeze(0).cuda()
+				input_var = torch.from_numpy(img_h.copy()).type(torch.FloatTensor).unsqueeze(0).to(device)
 				s = time.time()
-				e_out = model(input_var)              
+				e_out = model(input_var,Type = 1)              
 				e = time.time()   
 				print(input_var.shape)       
 				print(e-s)    
@@ -197,17 +158,34 @@ if __name__ == '__main__':
 				e_out = e_out.squeeze().cpu().detach().numpy()			               
 				e_out = chw_to_hwc(e_out) 
 			              
-				temp = np.concatenate((img_ccc,e_out), axis=1)			
-				cv2.imwrite(result_dir + '/' + testfiles[f][:-4] +'_%d_9'%(epoch)+'.png',np.clip(e_out*255,0.0,255.0))
-				cv2.imwrite('./MTRBNet/' + testfiles[f][:-4] +'_LYSNet.png',np.clip(e_out*255,0.0,255.0))
-        
-		ps,ss =  C_PSNR_SSIM()
+				e_out = cv2.resize(e_out,(w,h))				
+				cv2.imwrite(result_dir + '/' + testfiles1[f][:-4] +'_%d_9'%(epoch)+'.png',np.clip(e_out*255,0.0,255.0))
 
-		print('PSNR_%.4f_SSIM_%.4f'%(ps,ss))
+		for f in range(len(testfiles2)):
+			model.eval()
+			with torch.no_grad():
+				img = cv2.imread(test_dir2 + '/' + testfiles2[f])
+				h,w,c = img.shape
+				img_ccc = cv2.resize(img,(512,512)) / 255.0
+				img_h = hwc_to_chw(img_ccc)
+				input_var = torch.from_numpy(img_h.copy()).type(torch.FloatTensor).unsqueeze(0).to(device)
+				s = time.time()
+				e_out = model(input_var,Type=2)              
+				e = time.time()   
+				print(input_var.shape)       
+				print(e-s)    
+	             
+				e_out = e_out.squeeze().cpu().detach().numpy()			               
+				e_out = chw_to_hwc(e_out) 
+                
+				e_out = cv2.resize(e_out,(w,h))			              
+			
+				cv2.imwrite(result_dir + '/' + testfiles2[f][:-4] +'_%d_9'%(epoch)+'.png',np.clip(e_out*255,0.0,255.0))
+                
 		save_checkpoint({
 			'epoch': epoch + 1,
 			'state_dict': model.state_dict(),
-			'optimizer' : optimizer.state_dict()}, is_best=0,PSNR=ps,SSIM=ss)
+			'optimizer' : optimizer.state_dict()}, is_best=0,PSNR=0,SSIM=0)
 			
 			
 
